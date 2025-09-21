3dece4d146f38b86935ff4c0238e33ce971bc088
Yasmin Venceslau <yasmin.venceslau@hotmail.com>
2025-09-21 18:27:30 -0300

    certo

--- product/models/category.py
+++ product/models/category.py
@@ -1,7 +1,11 @@
+# product/models/category.py
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.name
+        return self.title
--- product/tests/test_models.py
+++ product/tests/test_models.py
@@ -4,7 +4,7 @@ from product.models import Product, Category
 @pytest.mark.django_db
 def test_create_product_with_category():
     # cria uma categoria
-    cat = Category.objects.create(name="Ficção")
+    cat = Category.objects.create(title="Ficção")
     
     # cria um produto e associa a categoria
     product = Product.objects.create(title="Livro Teste", price=50)
