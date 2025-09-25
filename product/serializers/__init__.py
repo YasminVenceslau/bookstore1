31904bbfd2a51ef37060d59655a0cec8f6dcceb8
Yasmin Venceslau <yasmin.venceslau@hotmail.com>
2025-09-25 13:34:42 -0300

    teste 2

--- product/serializers/__init__.py
+++ product/serializers/__init__.py
@@ -1,5 +1,6 @@
-from .category_serializer import CategorySerializer
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from .product_serializer import ProductSerializer
-
-
+from .category_serializer import CategorySerializer
 
--- product/serializers/category_serializer.py
+++ product/serializers/category_serializer.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from rest_framework import serializers
 
 from product.models.category import Category
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +15,5 @@ class CategorySerializer(serializers.ModelSerializer):
             'description',
             'active',
         ]
+        extra_kwargs = {'slug': {'required': False}}
+
