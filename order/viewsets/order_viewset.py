2926eae8518ae4f729fa694319386b2796489a43
Yasmin Venceslau <yasmin.venceslau@hotmail.com>
2025-09-25 18:38:15 -0300

    teste order

--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -1,10 +1,18 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from order.models import Order
-from order.serializers import OrderSerializer
-
+from order.serializers.order_serializer import OrderSerializer
 
 class OrderViewSet(ModelViewSet):
-
+    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     serializer_class = OrderSerializer
+<<<<<<< Updated upstream
     queryset = Order.objects.all().order_by("id")
+=======
+
+    def get_queryset(self):
+        return Order.objects.all().order_by("id")
+>>>>>>> Stashed changes
